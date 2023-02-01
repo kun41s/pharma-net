abd83245046c788c2ae474810f4ea7fa8da35b57
Kunal <kunal@Admins-MacBook-Pro.local>
2023-02-01 11:51:00 +0530

    contract updated

--- smart-contract/contracts/Pharma.sol
+++ smart-contract/contracts/Pharma.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.17;
 contract Pharma {
 
     enum Roles {
-        Manufacturer, Distributor, Retailer, Transporter, Consumer
+        Manufacturer, Distributor, Retailer, Transporter
     }
 
     enum Status {
         inTransit, delivered
     }
 
+    
     struct Company {
         uint256 CRN;
         address owner;
@@ -23,11 +24,11 @@ contract Pharma {
         bytes32 id;
         string name;
         uint256 serialNo;
-        address manufacturer;
+        uint256 manufacturer;
         uint256 mfgDate;
         uint256 expDate;
-        Roles currentOwner;
-        string shipment;
+        uint256 currentOwner;
+        uint256 shipment;
     }
 
     struct PurchaseOrder {
@@ -48,6 +49,7 @@ contract Pharma {
         bytes32 id;
         uint256 creator;
         string drugName;
+        uint256 serialNo;
         uint256 quantity;  // ListOfAssets
         uint256 transporter;
         Status status;
@@ -59,15 +61,25 @@ contract Pharma {
     }
 
     Roles role;
+    event OwnerChange(string drugName, uint256 serialNo, uint256 from, uint256 to );
     mapping (address => Company) companies;
-    mapping (address => Drug[]) public drugs;
+    mapping (string => mapping (uint256 => Drug)) public drugs;
+    mapping (address => Drug[]) drugsByAddress;
     // mapping (address => PurchaseOrder[]) shipmentOrders;
     mapping (uint256 => PurchaseOrder) purchaseOrders;
     mapping (uint256 => Shipment) public shipments;
 
     // "123", "abc", "la", 0
-    // 12345, abcde, la, 1
-    // 123456, trans, la, 3
+    // 1234, abcde, la, 1
+    // 123456, retailer, la, 2
+    // 12345, trans, la, 3
+    /**
+     * registerCompany() is used to register new entities on the ledger. 
+     * @param _crn uint256 Company Registration Number (CRN)
+     * @param _name string Name of the company
+     * @param _location string Location of the company
+     * @param _role Role Role of company {Manufacturer, Distributer, Retailer, Transporter}
+     */
     function registerCompany(uint256 _crn, string memory _name, string memory _location, Roles _role) public returns (bool) {
         require(msg.sender != companies[msg.sender].owner, "address is already owning a company");
         // myCompany.role = _role;
@@ -75,27 +87,49 @@ contract Pharma {
         return true;   
     }
 
-    // abc, 1234, 009, 0987, 123, 0
-    function addDrug(string memory _name, uint256 _serialNo, uint256 _mfgDate, uint256 _expDate, uint256 _companyCRN, Roles _currentOwner) public onlyCompany(msg.sender) returns (bool) {
-        require(_companyCRN == companies[msg.sender].CRN, "Company Not Registered");
-        require(_currentOwner == companies[msg.sender].role, "You must be manufacturer");
+    // abc, 123, 098, 0987
+    /**
+     * addDrug() is used by any organisation registered as a ‘manufacturer’ to register a new drug on the ledger.
+     * @param _name string Name of the product
+     * @param _serialNo uint serial number of drug
+     * @param _mfgDate uint256 Date of manufacturing of the drug
+     * @param _expDate uint256 Expiration date of the drug
+     */
+    function addDrug(string memory _name, uint256 _serialNo, uint256 _mfgDate, uint256 _expDate) public onlyCompany(msg.sender) returns (bool) {
+        uint256 CRN = companies[msg.sender].CRN;
+        require(CRN == companies[msg.sender].CRN, "Company Not Registered");
+        require(companies[msg.sender].role == Roles(0), "You must be manufacturer");
 
         bytes32 drugId = bytes32(sha256(abi.encodePacked(_name, _serialNo))); 
 
-        drugs[msg.sender].push(Drug(drugId, _name, _serialNo, msg.sender, _mfgDate, _expDate, Roles(0), ""));
+        drugs[_name][_serialNo] = Drug(drugId, _name, _serialNo, CRN, _mfgDate, _expDate, CRN, 0);
+        drugsByAddress[msg.sender].push(Drug(drugId, _name, _serialNo, CRN, _mfgDate, _expDate, CRN, 0));
         return true;
     }
 
-    function getDrugs() public view returns(Drug[] memory) {
-        return drugs[msg.sender];
+    function getDrugs(string memory _drugName, uint256 _serialNo) public view returns(Drug memory) {
+        return drugs[_drugName][_serialNo];
     }
 
     // 123, abc, 20
     function createPO( uint256 _sellerCRN, string memory _drugName, uint256 _quantity) public onlyCompany(msg.sender) returns (bool) {
         uint256 buyerCRN = companies[msg.sender].CRN;
         bytes32 id = sha256(abi.encodePacked(buyerCRN, _drugName));
-        require(buyerCRN != _sellerCRN, "Manufacturer can't create order");
-        require (companies[msg.sender].role != Roles(0), "Manufacturer cannot create purchase order");
+        Roles buyerRole = companies[msg.sender].role;
+            
+        require (buyerRole != Roles(0), "Manufacturer cannot create purchase order");
+        require (buyerRole != Roles(3), "Transporter cannot create purchase order");
+        require(buyerCRN != _sellerCRN, "Buyer cannot be a seller");
+
+        // TODO: 
+        // if (buyerRole == Roles(1)) {
+        //     // seller role must be 0
+        //     // require ();
+        // } else if (buyerRole == Roles(2)) {
+        //     // seller role must be 1
+        // } else if (buyerRole == Roles(3)) {
+        //     // seller role must be 2
+        // }
 
         // shipmentOrders[]
         // purchaseOrders[msg.sender].push(PurchaseOrder(id, _drugName, _quantity, buyerCRN, _sellerCRN));
@@ -103,37 +137,105 @@ contract Pharma {
         return true;
     }
 
-    // 12345, abc, 
-    function createShipment(uint256 _buyerCRN, string memory _drugName, uint256 _quantity, uint256 _transporterCRN) public onlyCompany(msg.sender) returns (bool) {
-        require(companies[msg.sender].role == Roles(0), "only manufacturer can create shipment order");
+    // 12345, abc, 123456 
+    function createShipment(uint256 _buyerCRN, string memory _drugName, uint256 _serialNo, uint256 _transporterCRN) public onlyCompany(msg.sender) returns (bool) {
+        // require(companies[msg.sender].role == Roles(0), "only manufacturer can create shipment order");
         // require(purchaseOrders[msg.sender].sellerCRN == companies[msg.sender].CRN, "You do not have any purchase order");
-        uint256 sellerCRN = companies[msg.sender].CRN;
-        require (purchaseOrders[_buyerCRN].sellerCRN == sellerCRN, "Order Not Found");
-        // string memory transporterName = companies[]
+        uint256 ownerCRN = companies[msg.sender].CRN;
+        Roles sellerRole = companies[msg.sender].role;
+        require (purchaseOrders[_buyerCRN].sellerCRN == ownerCRN, "Order Not Found");
+        require (sellerRole != Roles(3), "Transporter cannot create shipment order");
+        // TODO: check whether shipment is called or not
+        // Check whether drugname is same or not
+        require (sha256(abi.encodePacked(purchaseOrders[_buyerCRN].drugName)) == sha256(abi.encodePacked(_drugName)), "Order Not Found");
+        
+        uint256 quantity = purchaseOrders[_buyerCRN].quantity;
         bytes32 id = sha256(abi.encodePacked(_buyerCRN, _drugName));
         // bytes32 transporter = sha256(abi.encodePacked(_transporterCRN));    // Add transporter name;
-        shipments[_transporterCRN] = Shipment(id, companies[msg.sender].CRN, "abc", _quantity, _transporterCRN, Status(0));
+        // shipments[_transporterCRN].push(Shipment(id, ownerCRN, _drugName, _serialNo, quantity, _transporterCRN, Status(0)));
+        shipments[_transporterCRN] = Shipment(id, ownerCRN, _drugName, _serialNo, quantity, _transporterCRN, Status(0));
+        if (drugs[_drugName][_serialNo].currentOwner == ownerCRN) {
+            drugs[_drugName][_serialNo].currentOwner = _transporterCRN;
+            drugs[_drugName][_serialNo].shipment = _transporterCRN;
+        }
+        emit OwnerChange(_drugName, _serialNo, ownerCRN, _transporterCRN);
         return true;
     }
 
+    // 
     function updateShipment(uint256 _buyerCRN, string memory _drugName) public returns (bool) {
         uint256 transporterCRN = companies[msg.sender].CRN;
-        require(companies[msg.sender].CRN != _buyerCRN, "Manufacturer can't be buyer or transporter");
-        require(companies[msg.sender].CRN == transporterCRN, "Transporter Not Found");
-        if(shipments[transporterCRN].transporter == transporterCRN) {
-            if (sha256(abi.encodePacked(shipments[transporterCRN].drugName)) == sha256(abi.encodePacked(_drugName))) {
+        uint256 serialNo = shipments[transporterCRN].serialNo;
+        require(companies[msg.sender].CRN != _buyerCRN, "Buyer can't update shipment");
+        require(companies[msg.sender].role == Roles(3), "Only transporter can update shipment");
+        require(companies[msg.sender].CRN == shipments[transporterCRN].transporter, "Transporter Not Found");
+        require(shipments[transporterCRN].transporter == transporterCRN, "You do not have any shipment");
+        if (sha256(abi.encodePacked(shipments[transporterCRN].drugName)) == sha256(abi.encodePacked(_drugName))) {
+            require(shipments[transporterCRN].status == Status(0), "You do not have any shipment to update");
+            if (drugs[_drugName][serialNo].currentOwner == transporterCRN) {
+                drugs[_drugName][serialNo].currentOwner = _buyerCRN;
                 shipments[transporterCRN].status = Status(1);
-                return true;
+                // return true;
             }
         } else {
             return false;
         }
+        emit OwnerChange(_drugName, serialNo, transporterCRN, _buyerCRN);
         return true;
         // shipments[msg.sender] = Shipment
     }
 
-    function getShipments() public view returns(Shipment memory) {
+    function getShipments() public onlyCompany(msg.sender) view returns(Shipment memory) {
+        // require(companies[msg.sender].role == Roles(3));
         uint256 myCRN = companies[msg.sender].CRN;
+        if (shipments[myCRN].status != Status(0)) {
+            
+        } 
         return shipments[myCRN];
+    }
+
+    /**
+     * retailDrug() is called by the retailer while selling the drug to a consumer
+     * @param _drugName string
+     * @param _serialNumber uint256 serial number of drug
+     * @param _customerAadhar uint256
+     */
+    function retailDrug(string memory _drugName, uint256 _serialNumber, uint256 _customerAadhar) public onlyCompany(msg.sender) returns (bool) {
+        uint256 CRN = companies[msg.sender].CRN;
+        uint256 currentOwner = drugs[_drugName][_serialNumber].currentOwner;
+        require (companies[msg.sender].role == Roles(2), "You are not a retailer");
+        require (currentOwner == CRN, "You must be a current owner of drug");  // Retailer must be current owner of drug
+        if (currentOwner == CRN) {
+            drugs[_drugName][_serialNumber].currentOwner = _customerAadhar;
+            return true;
+        }
+        emit OwnerChange(_drugName, _serialNumber, CRN, _customerAadhar);
+        return true;
+    }
+
+    /**
+     * view the lifecycle of the product by fetching transactions from the blockchain.
+     * @param _drugName string
+     * @param _serialNo uint256
+     * @return Transaction id along with the details of the asset for every transaction associated with it.
+     */
+    // function viewHistory(string memory _drugName, uint256 _serialNo) public view returns (Drug memory) {
+    //     // return OwnerChang
+    // }
+
+    /**
+     * used to view the current state of the asset
+     * @param _drugName string
+     * @param _serialNo uint256
+     */
+    function viewDrugCurrentState(string memory _drugName, uint256 _serialNo) public view returns (Drug memory) {
+    //     // uint256 currentOwner = drugs[_drugName][_serialNo].currentOwner;
+    //     // Roles currentOwnerRole = companies[currentOwner].role;
+    //     // if (sha256(abi.encodePacked(drugs[_drugName][_serialNo].name)) == sha256(abi.encodePacked(_drugName)) && drugs[_drugName][_serialNo].serialNo == _serialNo) {
+    //     //     if (currentOwnerRole == Roles(0)) {
+
+    //     //     }
+    //     // }
+        return drugs[_drugName][_serialNo];
     }
 }
